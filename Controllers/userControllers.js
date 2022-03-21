f7db64dc29db01da5b53b4de119f22d80a6a283c
MarekPuu <marek.puurunen@gmail.com>
2022-03-21 18:52:06 +0200

    editprofile + changepassword route

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -4,9 +4,16 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const reqParams = require('../utils/requestParams');
 const ErrorHandler = require('../utils/ErrorHandler');
+const { validationResult } = require('express-validator');
 
 module.exports.register = async (req, res, next) => {
   try {
+    //validation middleware joka tulee express-validator packagesta
+    //toimii userRoutes kansiossa register routessa
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return ErrorHandler(400, errors.array());
+    }
     const { etunimi, sukunimi, sposti, nimimerkki, salasana } = req.body;
 
     const haveParams = reqParams(
@@ -22,15 +29,29 @@ module.exports.register = async (req, res, next) => {
 
     if (!haveParams) ErrorHandler(400, 'Params puuttuu');
 
-    const user = await Matkaaja.findOne({
-      $or: [{ sposti: sposti.toLowerCase() }, { nimimerkki: nimimerkki }],
+    //regex nimimerkki pitää suuria ja pieniä kirjaimia yhdenveroisina =>
+    //ei voi laittaa esim MarPuu ja marpuu nimimerkkiä enää mongodb
+
+    const regexNimimerkki = `^${nimimerkki}$`;
+    const user = await Matkaaja.find({
+      $or: [
+        { sposti: sposti.toLowerCase() },
+        { nimimerkki: { $regex: regexNimimerkki, $options: 'i' } },
+      ],
     });
 
-    if (user) {
+    //.find palauttaa aina arrayn vaikka se olisi tyhjä
+
+    if (user.length > 0) {
       const arr = [];
-      if (user.sposti === sposti.toLowerCase())
-        arr.push('Sposti on jo käytössä');
-      if (user.nimimerkki === nimimerkki) arr.push('nimimerkki on jo käytössä');
+
+      user.find((u) => {
+        if (u.sposti === sposti.toLowerCase())
+          arr.push('Sposti on jo käytössä');
+        if (u.nimimerkki.toLowerCase() === nimimerkki.toLowerCase())
+          arr.push('nimimerkki on jo käytössä');
+      });
+
       return res.status(400).json({ message: arr });
     }
 
@@ -112,6 +133,87 @@ module.exports.login = async (req, res, next) => {
         paikkakunta: user.paikkakunta,
       },
       accessToken,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.editProfile = async (req, res, next) => {
+  try {
+    const { etunimi, sukunimi, paikkakunta, esittely, sposti } = req.body;
+
+    const haveParams = reqParams(
+      { etunimi, sukunimi, paikkakunta, esittely, sposti },
+      req.body
+    );
+
+    if (!haveParams) ErrorHandler(400, 'Params puuttuu');
+
+    const user = await Matkaaja.findById(req.userID);
+
+    if (user.sposti !== sposti.toLowerCase()) {
+      //jos sposti vaihdetaan niin tarkistaa onko sposti vapaana
+      const spostiCheck = await Matkaaja.findOne({
+        sposti: sposti.toLowerCase(),
+      });
+
+      if (spostiCheck)
+        return res.status(400).json({ message: 'Sposti on jo käytössä' });
+    }
+
+    if (user) {
+      user.etunimi = etunimi.charAt(0).toUpperCase() + etunimi.slice(1);
+      user.sukunimi = sukunimi.charAt(0).toUpperCase() + sukunimi.slice(1);
+      user.paikkakunta = paikkakunta;
+      user.esittely = esittely;
+      user.sposti = sposti.toLowerCase();
+
+      const savedUser = await user.save();
+      res.status(200).json({
+        message: 'OK',
+        user: {
+          etunimi: savedUser.etunimi,
+          sukunimi: savedUser.sukunimi,
+          paikkakunta: savedUser.paikkakunta,
+          esittely: savedUser.esittely,
+          sposti: savedUser.sposti,
+        },
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports.changePassword = async (req, res, next) => {
+  try {
+    //validation middleware joka tulee express-validator packagesta
+    //toimii userRoutes kansiossa register routessa
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return ErrorHandler(400, errors.errors[0]['msg']);
+    }
+    const { uusiSalasana, salasana } = req.body;
+
+    const haveParams = reqParams({ uusiSalasana, salasana }, req.body);
+
+    if (!haveParams) ErrorHandler(400, 'Params puuttuu');
+
+    //etsitään käyttäjä requireAuth middlewaren antamasta id:stä joka otettiin JWT
+    const user = await Matkaaja.findById(req.userID);
+    //vertaillaan tietokannassa olevaa salasanaa requestissa olevaan salasanaan
+    const hashedPass = await bcrypt.compare(salasana, user.salasana);
+
+    if (!hashedPass)
+      return res.status(400).json({ message: 'Salasana on virheellinen' });
+
+    //Hashataan salasana ja tallennetaan tietokantaan
+    const hashPass = await bcrypt.hash(uusiSalasana, 10);
+    user.salasana = hashPass;
+    await user.save();
+
+    res.status(200).json({
+      message: 'OK',
     });
   } catch (error) {
     next(error);
--- Route/userRoute.js
+++ Route/userRoute.js
@@ -1,10 +1,32 @@
 const { Router } = require('express');
 const router = Router();
 const userController = require('../Controllers/userControllers');
+const { body } = require('express-validator');
+const requireAuth = require('../middleware/requireAuth');
 
-//http://localhost:3000/api/user
+//http://localhost:4000/api/user
 
-router.post('/register', userController.register);
+router.post(
+  '/register',
+  body('sposti').isEmail().withMessage('Sposti on virheellinen'),
+  body('salasana')
+    .isLength({ min: 5, max: 15 })
+    .withMessage('Salasanan täytyy olla 5-15 merkkiä pitkä'),
+  userController.register
+);
+
 router.post('/login', userController.login);
+
+//kaikki muut tiedot paitsi  salasana, kuva, nimimerkki
+router.put('/editprofile', requireAuth, userController.editProfile);
+
+router.put(
+  '/changepassword',
+  requireAuth,
+  body('uusiSalasana')
+    .isLength({ min: 5, max: 15 })
+    .withMessage('Salasanan täytyy olla 5-15 merkkiä pitkä'),
+  userController.changePassword
+);
 
 module.exports = router;
