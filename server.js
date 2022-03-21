6d8fb233cd92d66afa7a8c942fe279d205cacef6
MarekPuu <marek.puurunen@gmail.com>
2022-03-21 18:50:00 +0200

    Server käynnistys omassa kansiossa

--- server.js
+++ server.js
@@ -1,30 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const app = express();
-const requireAuth = require('./middleware/requireAuth');
+const app = require('./app');
 
-const userRoutes = require('./Route/userRoute');
+const port = process.env.PORT || 4000;
 
-app.use(express.json());
-
-//http://localhost:3000/
-
-app.use('/api/user', userRoutes);
-
-app.use('/privateRoute', requireAuth, (req, res) => {
-  res.status(200).json({ userid: req.userID, sposti: req.sposti });
-});
-
-app.use((error, req, res, next) => {
-  const { statusCode } = error;
-  console.log(error.message);
-  res.status(statusCode).json({ message: error.message });
-});
-
-mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
-  console.log('Yhdistetty databaseen');
-  app.listen(process.env.PORT || 3002, () => {
-    console.log(`Serveri on käynnissä portissa ${process.env.PORT || 3002}`);
-  });
+app.listen(port, () => {
+  console.log(`Serveri on käynnissä portissa ${port}`);
 });
