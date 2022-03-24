72e51612312a01f771aa64084fbf9bdf7b628f54
MarekPuu <marek.puurunen@gmail.com>
2022-03-24 18:36:49 +0200

    multer middleware kuvien vastaanottamiseen

---
+++ middleware/multer.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')
+    cb(null, true);
+  else cb(null, false);
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+module.exports = upload;
