78e3a19c593087cd9ada13f81f56a2cc5969bfbb
MarekPuu <marek.puurunen@gmail.com>
2022-03-24 20:00:34 +0200

    AWS_s3 config toimii

--- utils/AWS_s3.js
+++ utils/AWS_s3.js
@@ -1,5 +1,9 @@
 const AWS = require('aws-sdk');
-const s3Client = new AWS.S3();
+const s3Client = new AWS.S3({
+  accessKeyId: process.env.AWS_BUCKET_IAM_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_BUCKET_IAM_PRIVATE_KEY,
+  region: 'eu-north-1',
+});
 
 module.exports = { s3Client };
 
