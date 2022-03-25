3a76580f72be7170e6d2aadf22f140da26945bd8
MarekPuu <marek.puurunen@gmail.com>
2022-03-25 20:22:50 +0200

    CORS middleware

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
 const requireAuth = require('./middleware/requireAuth');
+const cors = require('cors');
 const ErrorHandler = require('./middleware/errorHandler');
 const userRoutes = require('./Routes/userRoutes');
 const matkakohdeRoutes = require('./Routes/matkakohteetRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 //http://localhost:4000/
 
