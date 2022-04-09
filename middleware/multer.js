cfd8f92c6887c3a16754f87cdf7b92296175fabd
MarekPuu <marek.puurunen@gmail.com>
2022-04-09 08:23:04 +0300

    Multeriin path.join

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,9 +1,10 @@
 const multer = require('multer');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads/');
+    cb(null, path.join(__dirname, '..', 'uploads'));
   },
   filename: function (req, file, cb) {
     cb(null, 'image-' + uuidv4() + '.jpeg');
